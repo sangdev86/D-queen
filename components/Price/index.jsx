2f71d9b68898a85acdd61e14574c043f8ec98455
sangdev86 <sangtran.dev86@gmail.com>
2022-06-27 00:29:25 +0700

    cart

--- components/Price/index.jsx
+++ components/Price/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { convertVND } from '../../Assets/utils/convertMoney';
 
-export default function Price({ sale, price }) {
+export default function Price({ sale, price, charge }) {
   return (
     <div id="price">
-      <span className="price-sale">{convertVND(sale)}</span>
-      <br />
-      <span className="none-price-sale">{convertVND(price)}</span>
+      {!charge ? (
+        <>
+          <span className="price-sale">{convertVND(sale)}</span>
+          <br />
+          <span className="none-price-sale">{convertVND(price)}</span>
+        </>
+      ) : (
+        <span className="charge">{convertVND(charge)}</span>
+      )}
     </div>
   );
 }
--- components/Table/index.jsx
+++ components/Table/index.jsx
@@ -1,25 +1,25 @@
-// import React from 'react';
+import React from 'react';
 
-// export default function Table() {
-//   return (
-//     <div id="table">
-//       <table style="">
-//         <tr>
-//           <th>Company</th>
-//           <th>Contact</th>
-//           <th>Country</th>
-//         </tr>
-//         <tr>
-//           <td>Alfreds Futterkiste</td>
-//           <td>Maria Anders</td>
-//           <td>Germany</td>
-//         </tr>
-//         <tr>
-//           <td>Centro comercial Moctezuma</td>
-//           <td>Francisco Chang</td>
-//           <td>Mexico</td>
-//         </tr>
-//       </table>
-//     </div>
-//   );
-// }
+export default function Table() {
+  return (
+    <div id="table">
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
