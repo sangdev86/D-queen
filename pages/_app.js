9bb387908b40388b0e3eb485f4eb329fb81e4c97
sangdev86 <sangtran.dev86@gmail.com>
2022-07-09 10:52:28 +0700

    SEO fix 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,8 @@
-// import Layout from '../Layout';
-// import { wrapper } from '../store';
-// import '../Assets/styles/main.scss';
-// import React from 'react';
-
-// function MyApp({ Component, pageProps }) {
-//   React.useEffect(() => {
-//     document
-//       .getElementsByTagName('HTML')[0]
-//       .setAttribute(
-//         'data-theme',
-//         localStorage.getItem('theme') || 'one'
-//       );
-//   });
-
-//   return <Layout props={<Component {...pageProps} />} />;
-// }
-
-// export default wrapper.withRedux(MyApp);
 import Layout from '../Layout';
 import { wrapper } from '../store';
 import '../Assets/styles/main.scss';
 import React from 'react';
 import App from 'next/app';
-
-// function MyApp({ Component, pageProps }) {
-//   React.useEffect(() => {
-//     document
-//       .getElementsByTagName('HTML')[0]
-//       .setAttribute(
-//         'data-theme',
-//         localStorage.getItem('theme') || 'one'
-//       );
-//   });
-
-//   return <Layout props={<Component {...pageProps} />} />;
-// }
 
 class MyApp extends App {
   // getInitialProps = wrapper.getInitialAppProps(
